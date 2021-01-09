76d7a0c3a13dbf7d2ee0fb8329e0cec5ddded1c6
takusuzu601 <takusuzu601@gmail.com>
2021-01-09 16:02:38 +0900

    検索システム雛形

--- app/Http/Livewire/Search.php
+++ app/Http/Livewire/Search.php
@@ -3,11 +3,25 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Livewire\WithPagination;
+use App\Models\User;
+ 
 
 class Search extends Component
 {
+    use WithPagination;
+    public $searchTerm;
+
     public function render()
     {
-        return view('livewire.search');
+        
+    	$query = '%'.$this->searchTerm.'%';
+
+    	return view('livewire.search', [
+    		'users'		=>	User::where(function($sub_query){
+    							$sub_query->where('name', 'like', '%'.$this->searchTerm.'%')
+    									  ->orWhere('email', 'like', '%'.$this->searchTerm.'%');
+    						})->paginate(10)
+    	]);
     }
 }
--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -1,3 +1,26 @@
 <div>
-    <h3>初めてのLivewire</h3>
+    <div class="container">
+	    <div class="row">
+	        <div class="col-md-12">	            
+	            <input type="text"  class="form-control" placeholder="Search" wire:model="searchTerm" />
+	            <table class="table table-bordered" style="margin: 10px 0 10px 0;">
+	                <tr>
+	                    <th>Name</th>
+	                    <th>Email</th>
+	                </tr>
+	                @foreach($users as $user)
+	                <tr>
+	                    <td>
+	                        {{ $user->name }}
+	                    </td>
+	                    <td>
+	                        {{ $user->email }}
+	                    </td>
+	                </tr>
+	                @endforeach
+	            </table>
+	            {{ $users->links() }}
+	        </div>
+	    </div>
+	</div>
 </div>
